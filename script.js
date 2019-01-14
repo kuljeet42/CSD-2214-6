58ed85ca24fd8f01791147dcbda6949022f0e6d7
kuljeet42 <46692876+kuljeet42@users.noreply.github.com>
2019-01-14 17:04:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//Name: Kuljeet kaur Chattha
+//Date: january 14,2019
+//Student: C0742139
+//Course: CSD 2214 Section_6
+
+alert("yeah!!!!!!!!!!!!!!!!!!! welcome to timer")
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+
+
+console.log(endDate.value);
+
